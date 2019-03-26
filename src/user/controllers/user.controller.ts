2d1763aac6b346046176df0f4a6bea80c339f5f3
Michał Borzęcki <ziqzzaq.web@gmail.com>
2019-03-26 10:49:32 +0100

    add authentication

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,7 +1,8 @@
-import {Body, Controller, Get, Post, UsePipes} from '@nestjs/common';
+import {Body, Controller, Get, Post, UseGuards, UsePipes} from '@nestjs/common';
 import {UserService} from "../services/user.service";
 import {UserDto} from "../models/user.dto";
 import {ValidationPipe} from "../../shared/validation.pipe";
+import {AuthGuard} from "../../shared/auth.guard";
 
 @Controller()
 export class UserController {
@@ -9,6 +10,7 @@ export class UserController {
     constructor(private userService: UserService) {}
 
     @Get('api/users')
+    @UseGuards(new AuthGuard())
     showAllUsers() {
         return this.userService.showAllUser();
     }
